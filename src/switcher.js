edd7573753e02e875b7acdd0d40df36d38e45945
Kirill Lee <cyril.lee1997@gmail.com>
2022-07-19 18:36:24 +0300

    pic-switch

--- src/switcher.js
+++ src/switcher.js
@@ -18,7 +18,7 @@ const switcher = {
         'url(' +
         galleryItems.map(e => e.original)[randomIntegerFromInterval(1, galleryItems.length - 1)] +
         ')';
-      refs.body.style.backgroundSize = 'cover';
+      refs.body.style.backgroundSize = 'contain';
     }, 2000);
 
     refs.startBtn.setAttribute('disabled', '');
